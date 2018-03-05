749d2ab0af5f74e8d6bc3fba33fcb4887cf2459c
TestingYG <31413025+TestingYG@users.noreply.github.com>
2018-03-05 10:10:39 -0500

    Update Speed1.java
    bug fixes

--- HW5/Question-1/Version-1/Speed1.java
+++ HW5/Question-1/Version-1/Speed1.java
@@ -4,7 +4,7 @@
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-public class Speed{
+public class Speed1{
 
   public static void main (String[] args ){
     long before = System.currentTimeMillis();
